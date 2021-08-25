1a05fd22f5496ab8b1e69e134426f0d4a4b6c7dc
snairn65 <89170436+snairn65@users.noreply.github.com>
2021-08-25 16:27:04 -0400

    Add files via upload

---
+++ August 25th.R
@@ -0,0 +1,13 @@
+# Author: Shone, Date: August 25, 2021, Purpose: Calculate the pearson correlation coefficient
+
+#Loading Library
+
+library("ggpubr")
+
+# Loading sample data
+
+my_data <- mtcars
+
+# Correlation value and plotting
+
+ggscatter(my_data, x = "mpg", y = "wt", add = "reg.line", conf.int = TRUE, cor.coef = TRUE, cor.method = "pearson", xlab = "Miles/(US) gallon", ylab = "Weight (1000 lbs)")
