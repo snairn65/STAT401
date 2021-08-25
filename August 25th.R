a5ad0aa17ff8c7ba8ed7d9c97fa941c9f14a1da9
snairn65 <89170436+snairn65@users.noreply.github.com>
2021-08-25 16:34:40 -0400

    Update August 25th.R

--- August 25th.R
+++ August 25th.R
@@ -11,3 +11,7 @@ my_data <- mtcars
 # Correlation value and plotting
 
 ggscatter(my_data, x = "mpg", y = "wt", add = "reg.line", conf.int = TRUE, cor.coef = TRUE, cor.method = "pearson", xlab = "Miles/(US) gallon", ylab = "Weight (1000 lbs)")
+
+# Generating a correlation value without plotting
+
+res <- cor.test(my_data$wt, my_data$mpg, method = "pearson")
