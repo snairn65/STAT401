090c4042bac8dcfb00b48c53234544dccd9faaa7
snairn65 <89170436+snairn65@users.noreply.github.com>
2021-10-15 16:31:49 -0400

    Update October 15th (In Class Assignment).R

--- October 15th (In Class Assignment).R
+++ October 15th (In Class Assignment).R
@@ -1,11 +1,11 @@
 #Author: Shone, Date: 15 October, 2021, Purpose: Create simple linear regresion model
 
-#Relationship: We want to predict the height of someone based on their weight
+#Relationship: We want to predict the weight based on height
 
-# Independent variable 'x'is the weigth
+# Independent variable 'x'is the height
 x <- c(151, 174, 138, 186, 128, 136, 179, 163, 152, 131)
 
-#Dependent variable 'y' is the height
+#Dependent variable 'y' is the weight
 y <- c(63, 81, 56, 91, 47, 57, 76, 72, 62, 48)
 
 # Create simple linear regression model 
@@ -14,7 +14,7 @@ relation <- lm(y~x)
 # Print out data
 print(relation)
 
-#y/HEIGHT= -38.4551 + (0.6746 x/time)
+#y/weight= -38.4551 + (0.6746 x/time)
 
 
-# for a weight of 170, the predicted height is 76.2269
+# for a height of 170, the predicted weight is 76.2269
