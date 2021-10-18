cac9278124db3a7821d504a9df57616e80246ba5
snairn65 <89170436+snairn65@users.noreply.github.com>
2021-10-18 16:53:25 -0400

    Add files via upload

--- October 8th.R
+++ October 8th.R
@@ -1,16 +1,33 @@
-#Author: Shone, Date: 8 Occtober, 2021, Purpose: Creating a simple linear regression model 
+Author: Shone Nairn, Date: Obtober 18th, 2021, Purpose:  Create Simple Linear regression model
 
+#Relationship: We want to predict the weight based on height
 
-#Relationship: we want to generate/test if happiness is graeter with more facebook usage or if its vice-versa
-
-#x is an independent variable (Facebook time usage in minutes)
+# Independent variable 'x'is the height
 x <- c(151, 174, 138, 186, 128, 136, 179, 163, 152, 131)
 
-# y is a dependent variable/outcome/ Happiness ranked on scale 0-10
-y <- c(1, 2, 9, 3, 4, 5, 7, 2, 2, 8)
+#Dependent variable 'y' is the weight
+y <- c(63, 81, 56, 91, 47, 57, 76, 72, 62, 48)
 
-# Creating model using function lm()
+# Create simple linear regression model 
 relation <- lm(y~x)
 
-#Print the output 
+# Print out data
 print(relation)
+
+#y/weight= -38.4551 + (0.6746 x/height)
+
+# for a height of 170, the predicted weight is 76.2269
+
+
+#Setting the work directory
+#mine is found on the file section of my workspace.
+
+# Give chart file a name
+png(file = "linearregression.png")
+
+# plot the chart
+plot(y,x,col = "blue",main = "Height & Weight Regression Analysis", abline(lm(y~x)),cex = 1.3,pch = 16,xlab = "Weight in Kg",ylab = "Height in cm")
+
+# Save file
+dev.off()
+
