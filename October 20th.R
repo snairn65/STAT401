50eba5aa91fe313097ac59e53b640928d3611fd7
snairn65 <89170436+snairn65@users.noreply.github.com>
2021-10-20 16:47:10 -0400

    Add files via upload

---
+++ October 20th.R
@@ -0,0 +1,17 @@
+Author: Shone Nairn, Date: October 20,2021, Purpose: Perform multiple linear regression
+on smaple dataset
+
+#install package #1
+install.packages("tidyverse")
+
+#load library 'tidyverse'
+library(tidyverse)
+
+#install package #2
+install.packages('datarium')
+
+#load library 'datarium'
+library(datarium)
+
+
+#load sample data from library 'tidyverse'
